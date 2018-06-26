6f59833b0ab56b686a7a827664ce2c1182123885
Simran Brucherseifer <simran@arangodb.com>
2018-06-26 16:26:10 +0200

    Add EOL message and include version number if available

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # gitbook-plugin-arangodb-outdated
 
-This gitbook plugin adds an obsolescence note at the top of each page on the website output.
-The message is currently hardcoded for the ArangoDB docs.
+This gitbook plugin adds an obsolescence note at the top of each page for the website output.
+The message is hardcoded for the ArangoDB docs.
 
 ## Usage
 
 Add the plugin and its dependency to your `book.json`:
 
-    ```js
-    {
-      "gitbook": "3.2.2",
-      "plugins": [
-        "callouts@git+https://github.com/Simran-B/gitbook-plugin-callouts.git",
-        "arangodb-outdated@git+https://github.com/Simran-B/gitbook-plugin-arangodb-outdated.git"
-      ]
-    }
-    ```
+```js
+{
+  "gitbook": "3.2.2",
+  "plugins": [
+    "callouts@git+https://github.com/Simran-B/gitbook-plugin-callouts.git",
+    "arangodb-outdated@git+https://github.com/Simran-B/gitbook-plugin-arangodb-outdated.git"
+  ]
+}
+```
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var fs = require('fs');
 var note = '';
+var version = '';
 
 module.exports = {
     hooks: {
         // called on each book & each language book
         'init': function() {
-            note = '{% hint \'warning\' %}\n' +
-                   'This documentation is outdated. Please see the most recent version here:\n' +
-                   '[**Latest Manual**](https://docs.arangodb.com/latest/Manual/)\n' +
-                   '{% endhint %}\n\n';
+            var versionMajorMinor = /(^v?\d+\.\d+)/.exec(book.version);
+            if (versionMajorMinor && versionMajorMinor[1]) {
+                version = versionMajorMinor[1];
+            }
+            note = '{% hint \'warning\' %}\n'
+            if (version) {
+                note += 'ArangoDB ' + version + 'reached End of Life (EOL) and is no longer supported.\n\n'
+            }
+            note += 'This documentation is outdated. Please see the most recent version here:\n' +
+                    '[**Latest Manual**](https://docs.arangodb.com/latest/Manual/)\n' +
+                    '{% endhint %}\n\n';
         },
         'page:before': function(page) {
             // append to the website renderer only
