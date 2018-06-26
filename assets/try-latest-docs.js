87af57768e4e5e6ca5efa0850b0f03fae1b8295d
Simran Brucherseifer <simran@arangodb.com>
2018-06-26 23:48:58 +0200

    Use last part from gitbook root (book name) plus root-relative path
    This allows to locally serve a parent folder of 'books', but assumes that the last folder of the root is Manual/AQL/HTTP/...

--- assets/try-latest-docs.js
+++ assets/try-latest-docs.js
@@ -1,8 +1,12 @@
 function replaceGenericLink() {
     var latestLink = document.querySelector("a.latest-docs");
-    var url = /^(https?:\/\/.+?)(\/devel|\d+\.\d+)?(\/.*)/.exec(window.location.href);
-    if (latestLink && url) {
-        latestLink.href = 'https://docs.arangodb.com/latest' + url[3];
+    var root = gitbook.state.root;
+    var url = window.location.href;
+    var idx = root.lastIndexOf('/', root.length-2);
+    if (latestLink && idx > 7 && url.indexOf(root) === 0) {
+        var book = root.slice(idx);
+        var path = book + url.slice(root.length);
+        latestLink.href = 'https://docs.arangodb.com/latest' + path;
         latestLink.innerHTML = '<strong>Try latest</strong>';
     }
 }
@@ -11,6 +15,6 @@ gitbook.events.bind('start', function(e, config) {
     replaceGenericLink();
 });
 
-gitbook.events.bind('page.change', function() {
+gitbook.events.bind('page.change', function(e) {
     replaceGenericLink();
 });
